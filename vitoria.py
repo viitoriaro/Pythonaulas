854998c9eae19b596b6fdaff77374d733bb3e372
viitoriaro <127975682+viitoriaro@users.noreply.github.com>
2023-05-19 11:49:38 -0300

    Add files via upload

---
+++ vitoria.py
@@ -0,0 +1,38 @@
+print("mensagem que eu quero", end="  ")
+
+print('mensagem 2')
+
+print('fulanoe é meu \"amigo\"')
+
+print("1+1")
+
+print("1 x 1 =" ,10/2)
+
+print("10 % 2" ,10%2)
+
+print("10³ =" , pow( 10,3)) 
+
+nome = ""
+
+print(nome)
+
+nome = 1+1
+
+print(nome)
+
+variavel = ""
+
+print(variavel)
+
+variavel = 100
+
+print(variavel)
+
+nome = 2
+
+nomee = 2.3
+
+print(int(nome + float(nomee)))
+
+
+
